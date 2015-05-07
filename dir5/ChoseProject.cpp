881447d72125238fe1d69d280d95a2a395b2e414
zhuqingqing <zhuqingqing@tsdig.com>
2015-05-07 10:44:37 +0800

    版本8  master 新建dir5  添加cpp h ui

---
+++ dir5/ChoseProject.cpp
@@ -0,0 +1,62 @@
+﻿#include "ChoseProject.h"
+
+#include <QAction>
+#include <QMessageBox>
+#include <QDebug>
+
+ChoseProject::ChoseProject(QWidget *parent) :QDialog(parent)
+{
+     setupUi(this);
+     d_project = new DomainProject();
+     handler = new ProjectDao();
+     QSqlTableModel *model = handler->selectInitialProj();
+     ProjsCurrent->setModel(model);
+     setTableHeader(":/TH_Proj_Open.txt",ProjsCurrent);
+     ProjsCurrent->hideColumn(0);
+     ProjsCurrent->resizeColumnsToContents();
+     tabWidget->setCurrentIndex(0);
+}
+
+
+void ChoseProject::setTableHeader(QString fileName,QTableView *tableview)
+{
+    QAbstractItemModel *model = tableview->model();
+    QFile file(fileName);
+    file.open(QIODevice::ReadOnly|QIODevice::Text);
+    QTextStream in(&file);
+    QString fileContent=in.readAll();
+    QStringList lines = fileContent.split(QString("\n"));
+    file.close();
+    for (int i = 0; i < lines.count(); i++)
+    {
+        model->setHeaderData(i, Qt::Horizontal,lines[i],Qt::EditRole);
+        qDebug()<<lines[i];
+        qDebug()<<model->headerData(i,Qt::Horizontal,Qt::EditRole);
+    }
+}
+ChoseProject::~ChoseProject()
+{
+}
+void ChoseProject::on_pushButton_Cancel_clicked()
+{
+    reject();
+}
+
+void ChoseProject::on_pushButton_Open_clicked()
+{
+   accept();
+}
+
+void ChoseProject::on_ProjsRecent_doubleClicked(const QModelIndex &index)
+{
+    QAbstractItemModel *model = ProjsRecent->model();
+    emit choseProj(model->data(model->index(index.row(),0)).toInt());
+    accept();
+}
+
+void ChoseProject::on_ProjsCurrent_doubleClicked(const QModelIndex &index)
+{
+    QAbstractItemModel *model = ProjsCurrent->model();
+    emit choseProj(model->data(model->index(index.row(),0)).toInt());
+    accept();
+}
---
+++ dir5/ChoseProject.h
@@ -0,0 +1,34 @@
+﻿#ifndef CHOSEPROJECT_H
+#define CHOSEPROJECT_H
+
+
+#include <QDialog>
+#include "ui_ChoseProject.h"
+#include "ProjectDao.h"
+#include "DomainProject.h"
+
+class ChoseProject : public QDialog,public Ui::ChoseProject
+{
+    Q_OBJECT
+
+  public:
+    DomainProject *d_project;
+    explicit  ChoseProject(QWidget *parent = 0);
+    ~ChoseProject();
+
+  private:
+    ProjectDao *handler;
+    void setTableHeader(QString filename,QTableView *tableview);
+
+  private slots:
+    void on_pushButton_Cancel_clicked();
+    void on_pushButton_Open_clicked();
+    void on_ProjsRecent_doubleClicked(const QModelIndex &index);
+    void on_ProjsCurrent_doubleClicked(const QModelIndex &index);
+
+signals:
+    void choseProj(int projpk);
+};
+
+
+#endif // CHOSEPROJECT_H
